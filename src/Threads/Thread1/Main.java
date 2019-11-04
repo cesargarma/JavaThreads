aced784adc710df61432c7bace7337587d7c4102
cesar <cesyser@gmail.com>
2019-11-04 13:40:55 +0100

    Implement runnable in another class, and merge both them on main

--- src/Threads/Thread1/Main.java
+++ src/Threads/Thread1/Main.java
@@ -5,8 +5,15 @@ public class Main {
         ThreadCreator miHijo = new ThreadCreator(1);
         ThreadCreator miHijo2 = new ThreadCreator(2);
         ThreadCreator miHijo3 = new ThreadCreator(3);
+
         miHijo.start();
         miHijo2.start();
         miHijo3.start();
+
+        for (int i = 0; i < 3; i++) {
+            ThreadRunnable miHilo = new ThreadRunnable();
+            Thread thread = new Thread(miHilo);
+            thread.start();
+        }
     }
 }
