b0742e46455bba856fd6bb479c6f980bdd6e8caa
cesar <cesyser@gmail.com>
2019-11-04 10:38:19 +0100

    2nd proyect ready

--- src/basic/ProcessAndInputStream.java
+++ src/basic/ProcessAndInputStream.java
@@ -1,14 +1,21 @@
 package basic;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ProcessAndInputStream {
     public static void main(String[] args) {
         try {
             Runtime runtime = Runtime.getRuntime();
-            //No funciona y no sepq, voy a investigar
-            Process process = runtime.exec("cmd");
+            Process process = runtime.exec("cmd /C dir");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while (bufferedReader.readLine() != null){
+                System.out.println(bufferedReader.readLine());
+            }
         } catch (IOException e) {
+            System.out.println("Error en el exec.");
             e.printStackTrace();
         }
     }
