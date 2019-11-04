4836eb0f490035f95a2d9e6b0b21077c02405a7e
cesar <cesyser@gmail.com>
2019-11-04 13:29:28 +0100

    i've been calling thread.run(); ... Well, It's fixed.

--- src/Threads/Thread1/Main.java
+++ src/Threads/Thread1/Main.java
@@ -3,10 +3,10 @@ package Threads.Thread1;
 public class Main {
     public static void main(String[] args) {
         ThreadCreator miHijo = new ThreadCreator(1);
-        miHijo.run();
         ThreadCreator miHijo2 = new ThreadCreator(2);
-        miHijo2.run();
         ThreadCreator miHijo3 = new ThreadCreator(3);
-        miHijo3.run();
+        miHijo.start();
+        miHijo2.start();
+        miHijo3.start();
     }
 }
